77d2ba75fafe2a79e839e06c16b31fa9c7fac922
rocean <rocean74@gmail.com>
2020-03-02 00:46:53 +0200

    fix git

---
+++ README.md
@@ -0,0 +1,23 @@
+# Eordaia.info site
+
+## Install npm dependencies
+
+```
+npm install
+```
+
+## Install laravel dependencies
+
+```
+compose update
+```
+
+## Run production environment
+
+```
+npm run watch
+```
+
+```
+php artisan serve --host 0.0.0.0
+```
---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Home from './views/Home'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/admin/',
+            name: 'home',
+            component: Home
+        }
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
