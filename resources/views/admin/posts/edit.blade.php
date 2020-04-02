dce5cd0aa0edf48e4bb80e8e5b9ddd96ef2b5d54
rocean <rocean74@gmail.com>
2020-04-03 00:05:05 +0300

    Edit tags

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -31,6 +31,26 @@
                                 <textarea class="form-control ckeditor" id="body" name="body">{{ $post->body }}</textarea>
                             </div>
 
+                            <div id="tagsContainer">
+                                <div class="input-group mb-3 no-gutters">
+                                    <label class="sr-only" for="tag">Tag</label>
+                                    <div class="input-group-prepend col-2">
+                                        <span class="input-group-text w-100">Tag</span>
+                                    </div>
+                                    <input type="text" max="255" v-model="tag" class="form-control col-8 px-2"
+                                           id="tag" name="tag">
+
+                                    <span class="btn btn-success col-2" @click="insertTag">Προσθήκη</span>
+
+                                    <input type="hidden" v-for="tag in tags" name="tags[]" :value="tag.id">
+
+                                </div>
+
+                                <div class="my-2 row">
+                                    <span class="my-1 mx-2 px-2 bg-primary text-light" v-for="tag in tags">{% tag.name %}</span>
+                                </div>
+                            </div>
+
                             <div class="input-group mb-3 no-gutters">
                                 <label class="sr-only" for="reference">Πηγή</label>
                                 <div class="input-group-prepend col-2">
@@ -97,5 +117,38 @@
             </div>
         </div>
     </div>
+
+@endsection
+
+@section('scripts')
+
+    <script>
+		let tags = new Vue({
+			el: '#tagsContainer',
+			delimiters: ['{%', '%}'],
+			data: {
+				tags: {!!
+                    json_encode($post->tags()->get()->map(function($item) {
+                        return ['id' => $item->id, 'name' => $item->name];
+                    }))
+                !!},
+				tag: ''
+			},
+			methods: {
+				insertTag(e) {
+					let myData = {
+						name: this.tag
+					}
+
+					axios.post('/api/tag', myData)
+						.then(response => {
+							this.tags.push({id: response.data.id, name: response.data.name})
+							this.tag = ''
+						})
+						.catch(e => console.log(e))
+				}
+			}
+		});
+    </script>
 
 @endsection
