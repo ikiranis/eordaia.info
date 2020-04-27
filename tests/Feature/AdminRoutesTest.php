b4917da214152487207239cc00f0b6de1fa62ae1
rocean <rocean74@gmail.com>
2020-04-27 18:01:18 +0300

    Admin routes test. Different class

---
+++ tests/Feature/AdminRoutesTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminRoutesTest extends TestCase
+{
+    protected static $user;
+    protected static bool $setUpRun = false;
+
+    /**
+     * Setup actions
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        if (!static::$setUpRun) {
+            static::$user = User::first();
+
+            static::$setUpRun = true;
+        }
+    }
+
+    /**
+     * Test admin page
+     *
+     * @return void
+     */
+    public function testAdminPage() : void
+    {
+        $response = $this->actingAs(static::$user, 'web')
+            ->get('/admin');
+
+        $response->assertStatus(200)
+            ->assertSee('Admin Page');
+    }
+
+    /**
+     * Test admin users page
+     *
+     * @return void
+     */
+    public function testAdminUsersPage() : void
+    {
+        $response = $this->actingAs(static::$user, 'web')
+            ->get('/admin/users');
+
+        $response->assertStatus(200)
+            ->assertSee('Χρήστες');
+    }
+
+    /**
+     * Test admin user create page
+     *
+     * @return void
+     */
+    public function testAdminUserCreatePage() : void
+    {
+        $response = $this->actingAs(static::$user, 'web')
+            ->get('/admin/users/create');
+
+        $response->assertStatus(200)
+            ->assertSee('Εισαγωγή χρήστη');
+    }
+
+    /**
+     * Test admin posts page
+     *
+     * @return void
+     */
+    public function testAdminPostsPage() : void
+    {
+        $response = $this->actingAs(static::$user, 'web')
+            ->get('/admin/posts');
+
+        $response->assertStatus(200)
+            ->assertSee('Δημοσιεύσεις');
+    }
+
+    /**
+     * Test admin post create page
+     *
+     * @return void
+     */
+    public function testAdminPostCreatePage() : void
+    {
+        $response = $this->actingAs(static::$user, 'web')
+            ->get('/admin/posts/create');
+
+        $response->assertStatus(200)
+            ->assertSee('Εισαγωγή δημοσίευσης');
+    }
+}
--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -2,28 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\User;
 use Tests\TestCase;
 
 class RoutesTest extends TestCase
 {
-    protected static $user;
-    protected static bool $setUpRun = false;
-
-    /**
-     * Setup actions
-     */
-    public function setUp() : void
-    {
-        parent::setUp();
-
-        if (!static::$setUpRun) {
-            static::$user = User::first();
-
-            static::$setUpRun = true;
-        }
-    }
-
     /**
      * Test admin page
      *
@@ -31,52 +13,6 @@ class RoutesTest extends TestCase
      */
     public function testAdminPage() : void
     {
-        $response = $this->actingAs(static::$user, 'web')
-            ->get('/admin');
-
-        $response->assertStatus(200)
-            ->assertSee('Admin Page');
-    }
-
-    /**
-     * Test admin users page
-     *
-     * @return void
-     */
-    public function testAdminUsersPage() : void
-    {
-        $response = $this->actingAs(static::$user, 'web')
-            ->get('/admin/users');
-
-        $response->assertStatus(200)
-            ->assertSee('Χρήστες');
-    }
-
-    /**
-     * Test admin posts page
-     *
-     * @return void
-     */
-    public function testAdminPostsPage() : void
-    {
-        $response = $this->actingAs(static::$user, 'web')
-            ->get('/admin/posts');
-
-        $response->assertStatus(200)
-            ->assertSee('Δημοσιεύσεις');
-    }
-
-    /**
-     * Test admin post create page
-     *
-     * @return void
-     */
-    public function testAdminPostCreatePage() : void
-    {
-        $response = $this->actingAs(static::$user, 'web')
-            ->get('/admin/posts/create');
-
-        $response->assertStatus(200)
-            ->assertSee('Εισαγωγή δημοσίευσης');
+        $this->assertTrue(true);
     }
 }
