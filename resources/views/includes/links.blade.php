64a5eff035af06b0031e5f45f3aa1610da49be3c
rocean <rocean74@gmail.com>
2020-04-29 00:06:17 +0300

    Display link

--- resources/views/includes/links.blade.php
+++ resources/views/includes/links.blade.php
@@ -1,6 +1,6 @@
 @if(!$post->links()->get()->isEmpty())
     <div class="row my-3">
-        <a href="{{ $post->links()->first() }}" class="ml-auto mr-auto" title="{{ $post->links()->first()->name }}">
+        <a href="{{ $post->links()->first()->url }}" class="ml-auto mr-auto" title="{{ $post->links()->first()->url }}">
             <span class="btn btn-outline-info">Περισσότερα στο <strong>{{ parse_url($post->links()->first()->url)['host'] }}</strong></span>
         </a>
     </div>
