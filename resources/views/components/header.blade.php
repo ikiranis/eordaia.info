82ced4e39738c093364fc628bf0520eaf07a3c4d
rocean <rocean74@gmail.com>
2020-05-17 23:59:38 +0300

    Links styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-    <a class="navbar-brand" href="{{ route('home') }}"><span class="mdi mdi-home-outline"></span>Eordaia.info</a>
+    <a class="navbar-brand" href="{{ route('home') }}"><span class="mdi mdi-home-outline" />Eordaia.info</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
