b6adff35f327890f8decda608697e866097137c2
rocean <rocean74@gmail.com>
2020-07-21 02:58:37 +0300

    navbar styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg container">
 
     <a class="navbar-brand" href="{{ route('home') }}"><span class="mdi mdi-home-outline" />Eordaia.info</a>
 
@@ -15,19 +15,19 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('contact') }}">Επικοινωνία</a>
             </li>
-            <li class="nav-item my-auto text-center">
-                <a href="{{ secure_url('/feed') }}" class="mx-1">
-                    <img src="/graphics/RSS.png" width="25" title="RSS News Feed">
-                </a>
+{{--            <li class="nav-item my-auto text-center">--}}
+{{--                <a href="{{ secure_url('/feed') }}" class="mx-1">--}}
+{{--                    <img src="/graphics/RSS.png" width="25" title="RSS News Feed">--}}
+{{--                </a>--}}
 
-                <a href="https://twitter.com/eordaia_info" class="mx-1">
-                    <img src="/graphics/twitter-logo.png" width="25" title="Twitter Page">
-                </a>
+{{--                <a href="https://twitter.com/eordaia_info" class="mx-1">--}}
+{{--                    <img src="/graphics/twitter-logo.png" width="25" title="Twitter Page">--}}
+{{--                </a>--}}
 
-                <a href="https://facebook.com/eordaiainfo-120099448002990/" class="mx-1">
-                    <img src="/graphics/facebook-logo.png" width="25" title="Facebook Page">
-                </a>
-            </li>
+{{--                <a href="https://facebook.com/eordaiainfo-120099448002990/" class="mx-1">--}}
+{{--                    <img src="/graphics/facebook-logo.png" width="25" title="Facebook Page">--}}
+{{--                </a>--}}
+{{--            </li>--}}
 
         </ul>
     </div>
