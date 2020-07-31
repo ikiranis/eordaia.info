9050539786bf9b6807dd610380b6a809f002a612
rocean <rocean74@gmail.com>
2020-08-01 02:44:00 +0300

    Pagination styling

--- app/View/Components/Posts/Paging.php
+++ app/View/Components/Posts/Paging.php
@@ -28,7 +28,7 @@ class Paging extends Component
     {
         return <<<'blade'
             <div class="row col-lg-6 col-12 ml-auto mr-auto">
-                <div class="ml-auto mr-auto">
+                <div class="pagination ml-auto mr-auto">
                     {{ $posts->links() }}
                 </div>
             </div>
