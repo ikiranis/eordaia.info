cbc369f38fe910aad4c9cb0d12a3ee3b42dbaab5
rocean <rocean74@gmail.com>
2020-08-08 17:18:20 +0300

    Footer styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="card">
         <div class="card-body row align-middle">
 
-            <div class="col text-center font-weight-bold">Υπό κατασκευή</div>
+            <div class="col-12 col-md text-md-center font-weight-bold">Υπό κατασκευή</div>
 
-            <div class="col text-right my-auto">
+            <div class="col-12 col-md text-md-right my-auto">
                 <span>
                     Created by <a href="http://www.apps4net.eu"><img
                                 src="{{url('/graphics/apps4net_logo.jpg')}}"></a>
