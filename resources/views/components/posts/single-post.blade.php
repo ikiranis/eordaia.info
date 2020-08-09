4b91786c6be1074835e9eb0d3ed4bce508cbe72c
rocean <rocean74@gmail.com>
2020-08-09 23:30:23 +0300

    Styling

--- resources/views/components/posts/single-post.blade.php
+++ resources/views/components/posts/single-post.blade.php
@@ -2,7 +2,7 @@
 
 <article>
 
-    <div class="ml-4 row listLabel px-3 mb-1">
+    <div class="ml-4 row listLabel px-3 mb-2">
         <x-posts.categories class="mx-3" :post="$post" />
     </div>
 
