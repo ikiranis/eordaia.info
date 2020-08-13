266a5c95fdb49d947f2bd2902ef6b282f0f75cef
rocean <rocean74@gmail.com>
2020-08-13 17:26:00 +0300

    Fix spacing

--- app/View/Components/Posts/CoverPhoto.php
+++ app/View/Components/Posts/CoverPhoto.php
@@ -34,7 +34,7 @@ class CoverPhoto extends Component
     {
         return <<<'blade'
             <div class="col">
-                <img src="{{ $photoUrl }}" class="card-img">
+                <img src="{{ $photoUrl }}" class="card-img mb-1">
                  @if ($singlePost && (isset($photo->description) || $photo->referral))
                     <div class="photoLabel row mx-3 px-3 mb-1">
                         <div class="mx-auto row text-center">
