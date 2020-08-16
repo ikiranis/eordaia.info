7c4c8b997e88f2ad0d920b9347acfe5c33355af8
rocean <rocean74@gmail.com>
2020-08-17 02:04:15 +0300

    Styling

--- resources/views/components/posts/search.blade.php
+++ resources/views/components/posts/search.blade.php
@@ -3,8 +3,8 @@
 
         <div class="input-group mb-3 no-gutters mx-auto">
             <label class="sr-only" for="search">Αναζήτηση</label>
-            <input type="text" max="255" class="form-control col-8 px-2" id="search" name="search">
-            <button class="btn btn-medium-primary col-4">Αναζήτηση</button>
+            <input type="text" max="255" class="form-control col-md-7 col-12 px-2" id="search" name="search">
+            <button class="btn btn-medium-primary col-md-5 col-12">Αναζήτηση</button>
         </div>
 
         @csrf
