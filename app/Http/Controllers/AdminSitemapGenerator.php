5cc0696472055a22a7188020e42a96afa37c5615
rocean <rocean74@gmail.com>
2020-08-18 19:09:01 +0300

    Set for daily

--- app/Http/Controllers/AdminSitemapGenerator.php
+++ app/Http/Controllers/AdminSitemapGenerator.php
@@ -15,8 +15,8 @@ class AdminSitemapGenerator extends Controller
         $sitemap = App::make("sitemap");
 
         // add items to the sitemap (url, date, priority, freq)
-        $sitemap->add(URL::to('about'),  Carbon::now(), '0.8', 'monthly');
-        $sitemap->add(URL::to('contact'),  Carbon::now(), '0.8', 'monthly');
+        $sitemap->add(URL::to('about'),  Carbon::now(), '0.8', 'daily');
+        $sitemap->add(URL::to('contact'),  Carbon::now(), '0.8', 'daily');
 
         // get all posts from db
         $posts = DB::table('posts')->orderBy('created_at', 'desc')->get();
@@ -24,7 +24,7 @@ class AdminSitemapGenerator extends Controller
         // add every post to the sitemap
         foreach ($posts as $post)
         {
-            $sitemap->add(route('post', $post->slug), $post->updated_at, '1.0', 'monthly');
+            $sitemap->add(route('post', $post->slug), $post->updated_at, '1.0', 'daily');
         }
 
         // generate your sitemap (format, filename)
