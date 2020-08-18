b49233a5c0bcb606f5f2de12df5c1aa796c4a797
rocean <rocean74@gmail.com>
2020-08-18 22:50:43 +0300

    Styling

--- app/View/Components/Posts/Related.php
+++ app/View/Components/Posts/Related.php
@@ -34,8 +34,8 @@ class Related extends Component
                 <div class="mt-3 px-3">
                     @foreach ($relatedPosts as $post)
                         <div class="row mb-3 col-12">
-                            <div class="my-auto col-12 col-md-auto px-3"><img src="{{ $post->photos->first()->smallPhotoUrl }}"></div>
-                            <div class="col my-auto col-12 col-md-auto">
+                            <div class="my-auto col-12 col-md-auto px-3 text-center text-md-left"><img src="{{ $post->photos->first()->smallPhotoUrl }}"></div>
+                            <div class="col my-auto col-12 col-md">
                                 <div><a href="{{ url($post->slug) }}">{{ $post->title }}</a></div>
                                 <div>{{ $post->updated_at->format('d/m/Y') }}</div>
                             </div>
