4f22255977d7c76325f0097f7a5683aecceed6ef
rocean <rocean74@gmail.com>
2020-09-08 00:42:26 +0300

    Better search icon

--- resources/views/components/posts/search.blade.php
+++ resources/views/components/posts/search.blade.php
@@ -1,10 +1,10 @@
 <div>
     <form method="GET" action="{{ route('search') }}">
 
-        <div class="input-group mb-3 no-gutters mx-auto">
+        <div class="input-group mb-3 no-gutters mx-auto search">
             <label class="sr-only" for="search">Αναζήτηση</label>
             <input type="text" max="255" class="form-control col-10 px-2" id="search" name="search">
-            <span class="btn-medium-primary col-2 text-center social-icon"><span class="mdi mdi-magnify"/></span>
+            <button class="btn-medium-primary col-2 text-center search-icon"><span class="mdi mdi-magnify"/></button>
         </div>
 
         @csrf
