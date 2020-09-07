6554af4608b6ad2d53bb602f2d8af24600ede252
rocean <rocean74@gmail.com>
2020-09-08 00:39:25 +0300

    Search icon

--- resources/views/components/posts/search.blade.php
+++ resources/views/components/posts/search.blade.php
@@ -3,8 +3,8 @@
 
         <div class="input-group mb-3 no-gutters mx-auto">
             <label class="sr-only" for="search">Αναζήτηση</label>
-            <input type="text" max="255" class="form-control col-md-7 col-12 px-2" id="search" name="search">
-            <button class="btn btn-medium-primary col-md-5 col-12">Αναζήτηση</button>
+            <input type="text" max="255" class="form-control col-10 px-2" id="search" name="search">
+            <span class="btn-medium-primary col-2 text-center social-icon"><span class="mdi mdi-magnify"/></span>
         </div>
 
         @csrf
