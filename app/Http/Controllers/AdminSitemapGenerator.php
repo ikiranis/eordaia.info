4efb6aa818f83afe6a3599897fa8ad18e6fe214c
rocean <rocean74@gmail.com>
2020-09-08 17:58:24 +0300

    Add categories and tags to sitemap

--- app/Http/Controllers/AdminSitemapGenerator.php
+++ app/Http/Controllers/AdminSitemapGenerator.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App;
+use App\Category;
+use App\Post;
+use App\Tag;
 use Carbon\Carbon;
-use DB;
 use URL;
 
 class AdminSitemapGenerator extends Controller
@@ -20,17 +22,34 @@ class AdminSitemapGenerator extends Controller
         $sitemap = App::make("sitemap");
 
         // add items to the sitemap (url, date, priority, freq)
-        $sitemap->add(URL::to('about'),  Carbon::now(), '0.8', 'daily');
-        $sitemap->add(URL::to('contact'),  Carbon::now(), '0.8', 'daily');
+        $sitemap->add(URL::to('about'),  Carbon::now(), null, null);
+        $sitemap->add(URL::to('contact'),  Carbon::now(), null, null);
+
+        $categories = Category::all();
+
+        // add every category to the sitemap
+        foreach ($categories as $category)
+        {
+            $sitemap->add(route('category', $category->slug), Carbon::now(), null, null);
+        }
+        $tags = Tag::all();
+
+        // add every tag to the sitemap
+        foreach ($tags as $tag)
+        {
+            $sitemap->add(route('tag', $tag->slug), Carbon::now(), null, null);
+        }
 
         // get all posts from db
-        $posts = DB::table('posts')->orderBy('created_at', 'desc')->get();
+        $posts = Post::orderBy('created_at', 'desc')->get();
 
         // add every post to the sitemap
         foreach ($posts as $post)
         {
-            $sitemap->add(route('post', $post->slug), $post->updated_at, '1.0', 'daily');
+            $sitemap->add(route('post', $post->slug), $post->updated_at, null, null);
         }
+
+
 
         // generate your sitemap (format, filename)
         $sitemap->store('xml', 'sitemaps/sitemap');
