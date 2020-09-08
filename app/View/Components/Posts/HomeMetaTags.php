49c0284e2dff6994ee30a5da012cc88d3f66b5c6
rocean <rocean74@gmail.com>
2020-09-09 02:42:34 +0300

    Metatags

--- app/View/Components/Posts/HomeMetaTags.php
+++ app/View/Components/Posts/HomeMetaTags.php
@@ -43,9 +43,10 @@ class HomeMetaTags extends Component
 
             <!-- Open Graph data -->
             <meta property="og:title" content="{{ config('app.name', 'Laravel') }}"/>
-            <meta property="og:type" content="home"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="{{ url('/') }}"/>
             <meta property="og:image" content="{{ url('/graphics/logo.png') }}"/>
-            <meta property="og:image:width" content="282">
+            <meta property="og:image:width" content="200">
             <meta property="og:description"
                   content="Ειδήσεις, εμπορικός οδηγός, αγγελίες κτλ από την Πτολεμαϊδα"/>
             <meta property="og:site_name" content="{{ config('app.name', 'Laravel') }}"/>
