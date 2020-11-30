d4c407eb90de446c4f2ebc3821c024231a5f8be6
rocean <rocean74@gmail.com>
2020-12-01 00:30:00 +0200

    Models editing

--- app/Business.php
+++ app/Business.php
@@ -2,10 +2,39 @@
 
 namespace App;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Business extends Model
 {
     use HasFactory;
+    use Uuids;
+
+    public $incrementing = false;
+
+    // Need to add this, for eager loading to work
+    // With this, the uuid used as string in queries. Otherwise, the uuid converted to number and queries
+    // doesn't work for some uuid's
+    protected $keyType = 'string';
+
+    // The attributes that are mass assignable
+    protected $fillable = [
+        'name',
+        'slug',
+        'address',
+        'email',
+        'bizcategory_id',
+        'customer_id',
+        'photo_id',
+        'valid_code'
+    ];
+
+    /**
+     * Relation to customers
+     */
+    public function business()
+    {
+        $this->belongsTo('App\Customer');
+    }
 }
--- app/Customer.php
+++ app/Customer.php
@@ -2,10 +2,24 @@
 
 namespace App;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
     use HasFactory;
+    use Uuids;
+
+    public $incrementing = false;
+
+    // Need to add this, for eager loading to work
+    // With this, the uuid used as string in queries. Otherwise, the uuid converted to number and queries
+    // doesn't work for some uuid's
+    protected $keyType = 'string';
+
+    // The attributes that are mass assignable
+    protected $fillable = [
+        'posts'
+    ];
 }
