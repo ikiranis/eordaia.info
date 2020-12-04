b8dbd1f2f4f100b821e900a883871730d616bede
rocean <rocean74@gmail.com>
2020-12-04 23:04:30 +0200

    Customer factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'posts' => 3,
+            'payment' => rand(1,10)
+        ];
+    }
+}
