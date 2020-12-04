83dc8762ce86e44d59e77fcbadaaea9b99086541
rocean <rocean74@gmail.com>
2020-12-04 23:09:22 +0200

    Business factory

---
+++ database/factories/BusinessFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Business;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BusinessFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Business::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'address' => $this->faker->address,
+            'email' => $this->faker->companyEmail,
+            'valid_code' => $this->faker->text(20)
+        ];
+    }
+}
